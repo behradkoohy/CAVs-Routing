7445381a4d2962c8c21b6d098f1c436f3097df72
Behrad Koohy <bk2g18@soton.ac.uk>
2022-05-26 11:23:36 +0100

    initial commit

---
+++ Simulation.py
@@ -0,0 +1,12 @@
+import os
+import sys
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("Please set SUMO_HOME")
+
+import sumolib
+import traci
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
